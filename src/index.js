4c69398e2f6ecb12b3b9b9cff0285545288caac3
Art Mazreku <artmazi@gmail.com>
2022-02-27 20:22:59 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,75 +1,88 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-
-
-
+import "./index.css";
 
 const HomeView = () => {
-  const [search, setSearch] = React.useState("")
-  const [hero, setHero] = React.useState()
-  
-  const Click = async(event, props) => {
-    event.preventDefault()
-    fetch(`https://www.superheroapi.com/api.php/10157652346894910/search/${search}`)
-      .then((res) => {
-        res.json()
-        .then((data) => {
-          setHero(data.results);
-        });
-      })
-      .catch((error) => {
-        console.error(error.message)
-      })
-  }
+  const [search, setSearch] = useState("");
+  const [heros, setHeros] = useState();
+  const [favoriteHeros, setFavoriteHeros] = useState([]);
 
-  const Local = () => {
+  const Click = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`https://www.superheroapi.com/api.php/10157652346894910/search/${search}`);
+      const data = await response.json();
+      setHeros(data.results);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-    const [item, setItem] = React.useState('')
+  const rujeNeLocalStorage = () => {
+    localStorage.setItem("favorite", JSON.stringify(favoriteHeros));
+  };
 
-    const data = fetch(`https://www.superheroapi.com/api.php/10157652346894910/644`)
-      .then(res =>{ 
-        res.json()
-        .then(data => {
-          setItem(data)
-        })
-      })
+  const fshije = (id) => {
+    let copy = [...favoriteHeros];
+    let index = copy.indexOf(id);
+    copy.splice(index, 1);
+    setFavoriteHeros(copy);
+  };
 
-        localStorage.setItem('name', data)
-        localStorage.setItem('full_name', data)
-        localStorage.setItem('alignment',data)
-        localStorage.setItem('race',data)
-        localStorage.setItem('image',data)
+  useEffect(() => {
+    if (favoriteHeros && favoriteHeros.length) {
+      rujeNeLocalStorage();
+    }
+    // eslint-disable-next-line
+  }, [favoriteHeros]);
 
-  }
-  
-  return(
+  useEffect(() => {
+    let localFavorite = localStorage.getItem("favorite");
+    if (localFavorite) {
+      localFavorite = JSON.parse(localFavorite);
+      if (localFavorite.length) {
+        setFavoriteHeros(localFavorite);
+      }
+    }
+  }, []);
+
+  return (
     <div>
-      <input placeholder="Movie Name" type="text" onChange={(e) => {setSearch(e.target.value)}} value={search} />
-      <button onClick={Click}>Search</button>
+      <form onSubmit={(event) => Click(event)}>
+        <input
+          placeholder="Movie Name"
+          type="text"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          value={search}
+        />
+
+        <button type="submit">Search</button>
+      </form>
       <div>
-        {hero &&
-          hero.length &&
-          hero.map((hero) => {
-            return(
-              <div key={hero.id}>
+        {heros &&
+          heros.length &&
+          heros.map((hero) => {
+            return (
+              <div key={hero.id} className="hero">
                 <h1 className="name">{hero.name}</h1>
                 <h1 className="full_name">{hero.biography[`full-name`]}</h1>
                 <h1 className="alignment">{hero.biography.alignment}</h1>
                 <h1 className="race">{hero.appearance.race}</h1>
                 <img src={hero.image.url} alt="" className="image" />
-                <button onClick={Local}>+</button>
+                {favoriteHeros.includes(hero.id) ? (
+                  <button onClick={() => fshije(hero.id)}>-</button>
+                ) : (
+                  <button onClick={() => setFavoriteHeros([...favoriteHeros, hero.id])}>+</button>
+                )}
               </div>
-            )
-        })}
+            );
+          })}
       </div>
     </div>
   );
-}
+};
 
-
-ReactDOM.render(
-  <HomeView/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<HomeView />, document.getElementById("root"));
