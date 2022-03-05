f43bf4a51edb3161888b1cfd0dfe825acff62486
Ismet Hamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2022-03-05 18:58:00 +0100

    Add files via upload

--- src/components/favorite.js
+++ src/components/favorite.js
@@ -3,85 +3,48 @@ import {
     useEffect
 } from 'react'
 
+import './favorite.css'
+
+
+const removeItem = (oldFavs = [], id) => {
+    const favoriteIds = JSON.parse(localStorage.getItem("favorite") || []);
+    const newFavorites = favoriteIds.filter(fId => fId !== id)
+    localStorage.setItem("favorite", JSON.stringify(newFavorites))
+
+    return oldFavs.filter(fav => fav.id !== id)
+}
 
 export const FavoriteView = () => {
-    const [favoriteHero, setFavoriteHero] = useState([])
-    const Remove = (id) => {
-        let getHero = [...favoriteHero]
-        let getHeroIndex = getHero.indexOf(id)
-        getHero.splice(getHeroIndex, 1)
-        setFavoriteHero(getHero)
+    const [favorites, setFavorites] = useState([]);
+    const remove = (id) => {
+        setFavorites(oldFavs => removeItem(oldFavs, id))
     }
-    const LocalStorage = () => {
-        localStorage.setItem("favorite", JSON.stringify(favoriteHero))
-    }
+
     useEffect(() => {
-        if (favoriteHero && favoriteHero.length) {
-            LocalStorage()
-        }
-    }, [favoriteHero])
-    useEffect(() => {
-        let getFavoriteFromLS = localStorage.getItem("favorite")
-        if (getFavoriteFromLS) {
-            getFavoriteFromLS = JSON.parse(getFavoriteFromLS)
-            if (getFavoriteFromLS.length) {
-                setFavoriteHero(getFavoriteFromLS)
-            }
-        }
+        const favoriteIds = JSON.parse(localStorage.getItem("favorite") || []);
+
+        favoriteIds.forEach(id => {
+            fetch(`https://www.superheroapi.com/api.php/10157652346894910/${id}`)
+                .then(res => res.json())
+                .then(res => setFavorites(oldItems => ([res, ...oldItems]))
+        )
+    })
     }, [])
-    useEffect(() => {
-        let getId = JSON.parse(localStorage.getItem("favorite"));
-        console.log(getId)
-        (async () => {
-          const heros = [];
-        //   console.log(heros)
-          for await (const id of getId) {
-            await (async () => {
-              const response = await fetch(`https://www.superheroapi.com/api.php/10157652346894910/${id}`);
-            //   console.log(response);
-              const data = await response.json();
-              heros.push(data.id);
-              console.log(heros)
-            })();
-          }
-          setFavoriteHero(heros);
-        })();
-
-// const heros = []
-        // for(const id of getId) {
-        //     const response = fetch(`https://www.superheroapi.com/api.php/10157652346894910/${id}`)
-        //         .then(res => res.json()
-        //         .then(data => heros.push(data)))
-        //         console.log(response)
-        // }
-        // setFavoriteHero(heros)
-
-      }, []);
 
     return(
-    <div>
+    <div className='parent'>
         <div>
-            {favoriteHero &&
-                favoriteHero.length &&
-                    favoriteHero.map((hero, index) => {
-                    return(
-                        <div key={index}>
-                            <h1 className="name">{hero}</h1>
-                            {/* <h1>{hero.biography['full-name']}</h1> */}
-                            {/* <h1>{hero.biography['full-name']}</h1>
-                            <h1 className="full_name">{hero.biography[`full-name`]}</h1>
+            {favorites.map(hero =>(
+                        <div key={hero.id} className='container'>
+                            <img src={hero.image.url} alt="" className="image" />
+                            <h1 className="name">{hero.name}</h1>
+                            <h1>{hero.biography['full-name']}</h1>
                             <h1 className="alignment">{hero.biography.alignment}</h1>
                             <h1 className="race">{hero.appearance.race}</h1>
-                            <img src={hero.image.url} alt="" className="image" />
-
-                            {favoriteHero.includes(hero.id)? (
-                                <button style={{backgroundColor: 'white', color: 'white', border: 'none'}}>-</button>
-                            ): (
-                                <button onClick={() => {Remove(hero.id)}}>-</button>
-                            )} */}
+                            <button onClick={() => remove(hero.id)}>-</button>
                         </div>
                     )
-                })}
+                )}
         </div>
     </div>
     );
--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-
+import './home.css';
 
 export const HomeView = () => {
     const [search, setSearch] = useState("")
@@ -19,15 +19,12 @@ export const HomeView = () => {
         })
     }
   
-    const LocalStorage = () => {
-      localStorage.setItem("favorite", JSON.stringify(favoriteHero))
-    }
-    
     useEffect(() => {
       if(favoriteHero && favoriteHero.length) {
-        LocalStorage()
+        localStorage.setItem("favorite", JSON.stringify(favoriteHero))
       }
     }, [favoriteHero])
+
     useEffect(() => {
       let getFavoriteFromLS = localStorage.getItem("favorite")
       if(getFavoriteFromLS) {
@@ -38,26 +35,26 @@ export const HomeView = () => {
       }
     }, [])
     return(
-      <div>
-        <form onSubmit={(event) => Click(event)}>
-        <input placeholder="Movie Name" type="text" onChange={(e) => {setSearch(e.target.value)}} value={search} />
-        <button type="submit">Search</button>
+      <div className='parent'>
+        <form className='form' onSubmit={(event) => Click(event)}>
+        <input placeholder="Movie Name" type="text" className='searchInput' onChange={(e) => {setSearch(e.target.value)}} value={search} />
+        <button type="submit" className='searchBtn'>Search</button>
         </form>
         <div>
           {heros &&
             heros.length &&
             heros.map((hero) => {
               return(
-                <div key={hero.id}>
+                <div key={hero.id} className='container'>
+                  <img src={hero.image.url} alt="" className="image" />
                   <h1 className="name">{hero.name}</h1>
                   <h1 className="full_name">{hero.biography[`full-name`]}</h1>
                   <h1 className="alignment">{hero.biography.alignment}</h1>
                   <h1 className="race">{hero.appearance.race}</h1>
-                  <img src={hero.image.url} alt="" className="image" />
                   {favoriteHero.includes(hero.id)? (
-                    <button style={{backgroundColor: 'white', color: 'white', border: 'none'}}>+</button>
+                    null
                   ): (
-                    <button onClick={() => {setFavoriteHero([...favoriteHero, hero.id])}}>+</button>
+                    <button onClick={() => {setFavoriteHero([ hero.id, ...favoriteHero])}}>+</button>
                   )}
                 </div>
               )
